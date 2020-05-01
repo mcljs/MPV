ff7e5231c9de597e1c981cc8aa032671d3b468d7
mcljs <mcljs15@gmail.com>
2020-05-01 18:29:31 -0400

    Updates

---
+++ precache-manifest.2a4e937d26a0f228e008fac9cbee7a3f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3874b1d4cb05fb3a3dcf91c5f362fb1",
+    "url": "/MPV/index.html"
+  },
+  {
+    "revision": "d437c8845a77b2583b95",
+    "url": "/MPV/static/css/main.f33eb281.chunk.css"
+  },
+  {
+    "revision": "f3d4c5244611e07ff7eb",
+    "url": "/MPV/static/js/2.840af288.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/MPV/static/js/2.840af288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d437c8845a77b2583b95",
+    "url": "/MPV/static/js/main.61bf0cff.chunk.js"
+  },
+  {
+    "revision": "3a0b873175eb296ee7c6",
+    "url": "/MPV/static/js/runtime-main.03cb293c.js"
+  },
+  {
+    "revision": "238b3724d1e36f0ceec876f9889c8c88",
+    "url": "/MPV/static/media/24abril.238b3724.jpg"
+  },
+  {
+    "revision": "98bbbcb090ce51f1077d10a7f4e35535",
+    "url": "/MPV/static/media/25abril.98bbbcb0.jpg"
+  },
+  {
+    "revision": "b88d346810bf741758090a0842e1ba22",
+    "url": "/MPV/static/media/28abril.b88d3468.jpg"
+  },
+  {
+    "revision": "b00a3d4396732e7f69336cfbb928dbe9",
+    "url": "/MPV/static/media/30abril.b00a3d43.jpg"
+  },
+  {
+    "revision": "2602e3bc793f0c1a8151f484a41aeb77",
+    "url": "/MPV/static/media/Banner.2602e3bc.jpg"
+  },
+  {
+    "revision": "b4cb65e1dabe776f5de307f36a1f9779",
+    "url": "/MPV/static/media/icon-pyv.b4cb65e1.png"
+  },
+  {
+    "revision": "546ef0fde5699a8a0f0eccecd0ffc010",
+    "url": "/MPV/static/media/showcase.546ef0fd.png"
+  }
+]);
